5d7fcc1a2be3467f8f6c01958afc80b8f2a3e4b5
vitoriaalbuquerqueee <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-06-30 15:33:02 -0300

    Refactor ServicoTemporario constructor and add methods

--- ServicoTemporario.java
+++ ServicoTemporario.java
@@ -2,13 +2,29 @@ package ECommerce_Functions;
 
 public class ServicoTemporario extends Servico {
 	
-	public ServicoTemporario(String nome, float preco, String funcao, int turno) {
-	super( nome,  preco, funcao, turno);
-	
-	
-	
-	
-}
-}
+		
+		int diasContrato;
+		
+		
+		public ServicoTemporario(int id, String nome, float preco, String funcao, int turno, int horasTrabalhadas, int diasContrato) {
+			super(id, nome,  preco, funcao, turno);
+			this.diasContrato = diasContrato;
+		
+		
+	}
+		public int getdiasContrato() {
+			return diasContrato;
+		}
+	     public void setdiasContratos(int diasContrato) {
+		    this.diasContrato = diasContrato;
+	}
+	 
+	     @Override
+	     public void visualizar() {
+	       super.visualizar();
+	     System.out.println("Quantidade de dias Contratatados: " + this.diasContrato + "Dias");
+
+	}
+	}
 
 
