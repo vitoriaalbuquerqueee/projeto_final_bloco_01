e57f6b2b3eb4d665ef8711d3abc7df220892bc09
vitoriaalbuquerqueee <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-06-30 15:35:16 -0300

    Implement service registration and listing methods

--- ServicoController.java
+++ ServicoController.java
@@ -1,5 +1,22 @@
 package ECommerce_Functions;
 
+import java.util.ArrayList;
+
 public class ServicoController {
 
+
+    private ArrayList<Servico> listaServicos = new ArrayList<>();
+
+
+    public void cadastrar(Servico servico) {
+        listaServicos.add(servico);
+        System.out.println("Serviço cadastrado com sucesso!");
+    }
+
+    
+    public void listarTodas() {
+        for (Servico s : listaServicos) {
+            s.visualizar();
+        }
+    }
 }
