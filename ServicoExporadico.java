f46344e7bcc9e5a5e399a3779f09ff77f57f058d
vitoriaalbuquerqueee <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-06-30 15:32:09 -0300

    Add hours worked attribute to ServicoExporadico

--- ServicoExporadico.java
+++ ServicoExporadico.java
@@ -2,8 +2,25 @@ package ECommerce_Functions;
 
 public class ServicoExporadico extends Servico {
 	
-	public ServicoExporadico(String nome, float preco, String funcao, int turno) {
-	super( nome,  preco, funcao, turno);
+	private int horasTrabalhadas;
 	
+	public ServicoExporadico(int id, String nome, float preco, String funcao, int turno, int horasTrabalhadas) {
+	super(id, nome,  preco, funcao, turno);
+	this.horasTrabalhadas = horasTrabalhadas;
+	
+	
+}
+	public int gethorasTrabalhadas() {
+		return horasTrabalhadas;
+	}
+     public void sethorasTrabalhadas(int horasTrabalhadas) {
+	    this.horasTrabalhadas = horasTrabalhadas;
+}
+ 
+     @Override
+     public void visualizar() {
+       super.visualizar();
+     System.out.println("Horas Contratatadas: " + this.horasTrabalhadas + "horas");
+
 }
 }
